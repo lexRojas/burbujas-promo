fa73a07d5098f8bede26808e9dee7c22c1327543
lexrojas <legalcr.rojas@gmail.com>
2024-11-18 16:55:43 -0600

    form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,31 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Image from "next/image";
+import burbujas from "../../public/burbujas-logo.png"
+
+import whatsap from "../../public/whatsapp.png"
+import instagram from "../../public/instagram.png"
+import facebook from "../../public/facebook.png"
+import Style from "./Text.module.css"
+import { Oswald, Roboto_Condensed } from 'next/font/google';
+
+const oswald = Oswald(
+  {
+    weight: '500',
+    subsets: ['latin'],
+    style: 'normal',
+
+  }
+)
+
+const roboto_condensed = Roboto_Condensed({
+  weight: '500',
+  subsets: ['latin'],
+  style: 'normal'
+
+})
+
 
 export const metadata: Metadata = {
   title: "Mundo Burbujas",
@@ -25,10 +39,67 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className=" antialiased" >
+        <div className="bubbles-container">
+
+          <div className="bubble"/>
+          <div className="bubble"/>
+          <div className="bubble"/>
+          <div className="bubble"/>
+          <div className="bubble"/>
+
+
+          <div className="grid grid-rows-[auto_auto_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-6  sm:p-20 ">
+
+            <div className="row-start-1 ">
+              <Image
+                src={burbujas}
+                alt="Burrbujas"
+                style={{ width: 'clamp(100px,600px,600px)' }}
+                className="logo-luminoso"
+              >
+              </Image>
+              <div className={Style.textcontainer} >
+                <p className={` ${oswald.className} ${Style.textmove}`}  >¡Los momentos alegres comienzan aquí!</p>
+              </div>
+
+            </div>
+            <div className="bar flex row-start-2 w-full justify-center align-top ">
+              <ul className={` ${roboto_condensed.className} flex flex-row  space-x-2  md:space-x-5  md:text-xl `}>
+                <li className="flex">JUGUETERIA</li>
+                <li>ELECTRÓNICOS</li>
+                <li>LIBRERÍA</li>
+              </ul>
+
+
+            </div>
+            <main className="flex flex-col gap-8 row-start-3 items-center w-full  ">
+
+              {children}
+
+            </main>
+            <footer className="row-start-4 flex  flex-row  pt-6 gap-6 flex-wrap items-center justify-center">
+              <div className="flex flex-shrink items-center gap-3">
+
+                <Image className="img-luminosa" alt="Whatsap" src={whatsap} style={{ width: 'clamp(10px,25px,25px)' }} ></Image>
+                <span className={`${roboto_condensed.className} text-black hover:text-blue-800`} style={{ fontSize: 'clamp(0.5rem, 2.5vw, 2rem)' }}> Whatsap </span>
+              </div>
+
+              <div className="flex items-center gap-3">
+                <Image className="img-luminosa" alt="Whatsap" src={facebook} style={{ width: 'clamp(10px,25px,25px)' }} ></Image>
+                <span className={`${roboto_condensed.className} text-black hover:text-blue-800`} style={{ fontSize: 'clamp(0.5rem, 2.5vw, 2rem)' }}> Facebook </span>
+              </div>
+              <div className="flex items-center gap-3">
+                <Image className="img-luminosa" alt="Whatsap" src={instagram} style={{ width: 'clamp(10px,25px,25px)' }} ></Image>
+                <span className={`${roboto_condensed.className} text-black hover:text-blue-800`} style={{ fontSize: 'clamp(0.5rem, 2.5vw, 2rem)' }} > Instagram </span>
+              </div>
+
+
+            </footer >
+          </div >
+
+
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,139 +1,51 @@
-import Image from "next/image";
-import burbujas from "../../public/burbujas-logo.png"
-
-import whatsap from "../../public/whatsapp.png"
-import instagram from "../../public/instagram.png"
-import facebook from "../../public/facebook.png"
-import Style from "./Text.module.css"
-import img1 from "../../public/stock/image1.jpg"
-
-
-import { Oswald, Roboto_Condensed } from 'next/font/google';
+import Link from "next/link";
 import ImageSliderReact from "../../components/ImageSliderReact";
 
-
-
-
-
-const oswald = Oswald(
-  {
-    weight: '500',
-    subsets: ['latin'],
-    style: 'normal',
-
-  }
-)
-
-
-const roboto_condensed = Roboto_Condensed({
-  weight: '500',
-  subsets: ['latin'],
-  style: 'normal'
-
-})
-
-
 export default function Home() {
-
-  console.log(img1.src)
 
   const imageLinks = [
     { id: 0, src: "/stock/image1.jpg", link: 'https://example.com/1' },
     { id: 1, src: "/stock/image2.jpg", link: 'https://example.com/1' },
     { id: 2, src: "/stock/image3.jpg", link: 'https://example.com/1' },
     { id: 3, src: "/stock/image4.jpg", link: 'https://example.com/1' },
-
-
-
-
   ];
 
   return (
+    <>
+      <div className="flex flex-row">
+        <ImageSliderReact images={imageLinks} />
+      </div>
 
-    <div className="bubbles-container">
 
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
-      <div className="bubble"></div>
+      <div className="flex flex-col gap-3 md:flex-row ">
+        <div className="flex items-center 
+                          justify-center  
+                          p-5
+                          main-button-1 ">
 
-      <div className="grid grid-rows-[auto_auto_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-6  sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-        <div className="row-start-1 ">
-          <Image
-            src={burbujas}
-            alt="Burrbujas"
-            style={{ width: 'clamp(100px,600px,600px)' }}
-            className="logo-luminoso"
-          >
-          </Image>
-          <div className={Style.textcontainer} >
-            <p className={` ${oswald.className} ${Style.textmove}`}  >¡Los momentos alegres comienzan aquí!</p>
-          </div>
-
-        </div>
-        <div className="bar flex row-start-2 w-full justify-center align-top ">
-          <ul className={` ${roboto_condensed.className} flex flex-row  space-x-2  md:space-x-5  md:text-xl `}>
-            <li className="flex">JUGUETERIA</li>
-            <li>ELECTRÓNICOS</li>
-            <li>LIBRERÍA</li>
-          </ul>
+          <Link href="/signup">
+            <p className="font-extrabold text-xl">Inscribirse </p>
+          </Link>
 
 
         </div>
-        <main className="flex flex-col gap-8 row-start-3 items-center  ">
-
-          <div className="flex flex-row">
-            <ImageSliderReact images={imageLinks} />
-          </div>
-
-
-          <div className="flex flex-col gap-3 md:flex-row ">
-            <div className="flex items-center 
+        <div className="flex items-center 
                           justify-center  
                           p-5
-                          bg-button-1 ">
+                          main-button-2 ">
 
-              <p className="font-extrabold text-xl">Inscribirse </p>
-            </div>
-            <div className="flex items-center 
+          <p className="font-extrabold   text-xl">Ver Puntos </p>
+        </div>
+        <div className="flex items-center 
                           justify-center  
                           p-5
-                          bg-button-2 ">
+                          main-button-3 ">
 
-              <p className="font-extrabold   text-xl">Ver Puntos </p>
-            </div>
-            <div className="flex items-center 
-                          justify-center  
-                          p-5
-                          bg-button-3 ">
+          <p className="font-extrabold   text-xl">Reglamento </p>
+        </div>
 
-              <p className="font-extrabold   text-xl">Reglamento </p>
-            </div>
+      </div>
 
-          </div>
-
-        </main>
-        <footer className="row-start-4 flex  flex-row  pt-6 gap-6 flex-wrap items-center justify-center">
-          <div className="flex flex-shrink items-center gap-3">
-
-            <Image className="img-luminosa" alt="Whatsap" src={whatsap} style={{ width: 'clamp(10px,25px,25px)' }} ></Image>
-            <span className={`${roboto_condensed.className} text-black hover:text-blue-800`} style={{ fontSize: 'clamp(0.5rem, 2.5vw, 2rem)' }}> Whatsap </span>
-          </div>
-
-          <div className="flex items-center gap-3">
-            <Image className="img-luminosa" alt="Whatsap" src={facebook} style={{ width: 'clamp(10px,25px,25px)' }} ></Image>
-            <span className={`${roboto_condensed.className} text-black hover:text-blue-800`} style={{ fontSize: 'clamp(0.5rem, 2.5vw, 2rem)' }}> Facebook </span>
-          </div>
-          <div className="flex items-center gap-3">
-            <Image className="img-luminosa" alt="Whatsap" src={instagram} style={{ width: 'clamp(10px,25px,25px)' }} ></Image>
-            <span className={`${roboto_condensed.className} text-black hover:text-blue-800`} style={{ fontSize: 'clamp(0.5rem, 2.5vw, 2rem)' }} > Instagram </span>
-          </div>
-
-
-        </footer >
-      </div >
-    </div >
+    </>
   );
 }
